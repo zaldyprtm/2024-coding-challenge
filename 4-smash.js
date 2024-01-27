5596479aedbcf738ffa9188843ee63e8941ec440
zaldyprtm <zaldyprtm@gmail.com>
2024-01-27 11:25:57 +0700

    day 4/366

---
+++ 4-smash.js
@@ -0,0 +1,27 @@
+// DAY 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+
+// function smash (words) {
+//     return words.join(' ');
+//  };
+
+// one liner
+const smash = (words) => words.join(' ');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ console.log(smash(['hello', 'word', 'dunia']));
