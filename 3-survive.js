a76c62b6083cca1d3d4bc2e3300f32c17f932b89
zaldyprtm <zaldyprtm@gmail.com>
2024-01-27 11:16:49 +0700

    day3/366

---
+++ 3-survive.js
@@ -0,0 +1,18 @@
+//DAY 3/366
+
+
+// function hero(bullets, dragons) {
+//  if (bullets / 2 >= dragons) {
+//     return true;
+//  } else {
+//     return false;
+//  }
+
+// }
+    
+// cara kedua
+function hero(bullets, dragons) {
+    return bullets / 2 >= dragons;
+}
+
+    console.log(hero(10, 5));
