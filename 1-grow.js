7de97578dfcbfdcd2af7dee7bfa72dcf736ccaa5
zaldyprtm <zaldyprtm@gmail.com>
2024-01-27 10:41:17 +0700

    update day 1

--- 1-grow.js
+++ 1-grow.js
@@ -1,12 +1,15 @@
-// function grow(x){
-//     let result = x[0];
-    
-//     for ( let i = 1; i < x.length; i++) {
-//         result *= x[i];
-//     }
+// DAY 1/366
 
-//     return result;
-// }
+
+function grow(x){
+    let result = x[0];
+    
+    for ( let i = 1; i < x.length; i++) {
+        result *= x[i];
+    }
+
+    return result;
+}
 
 
 // function grow(x) {
@@ -17,4 +20,4 @@
 
 // const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
 
-// console.log(grow([1, 2, 3, 4]));
+console.log(grow([1, 2, 3, 4]));
