28bc0990cf82339e51c1c37355bcb478421733a4
zaldyprtm <zaldyprtm@gmail.com>
2024-01-27 13:08:58 +0700

    day9/366

---
+++ 9-maxmin.js
@@ -0,0 +1,7 @@
+// 9/366
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+console.log(min([42, 54, 65, 87, 0, 1, 11, 111]));
+console.log(max([42, 54, 65, 87, 0]));
